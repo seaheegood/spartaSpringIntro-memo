20d725c561c7ec80e473c8665f39d9cd3d03016b
Sehee Hong <seaheehong@gmail.com>
2024-02-01 19:58:39 +0900

    Query Method 추가

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -1,23 +1,11 @@
 package com.sparta.memo.repository;
 
-import com.sparta.memo.dto.MemoRequestDto;
-import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
-import jakarta.persistence.EntityManager;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> { //memoRepository
-
+    List<Memo> findAllByOrderByModifiedAtDesc();
+//    List<Memo> findAllByUsername(String username);
 }
